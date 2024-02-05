90e323f73e880dd3f7aa28922da237477a1643e6
overlordpro-sys <64398319+overlordpro-sys@users.noreply.github.com>
2024-02-04 18:17:46 -0800

    Basic serverside no RPC working, except client

---
+++ Assets/Prefabs/Player/PlayerNetwork.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Components;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Player {
+    public class PlayerNetwork : NetworkBehaviour {
+        [SerializeField] private Rigidbody2D _body;
+
+        private Vector2 _moveVector;
+        [SerializeField] private float _acceleration = 5f;
+        public override void OnNetworkSpawn() {
+            //if (IsOwner) {
+
+            //}
+        }
+
+        private void FixedUpdate() {
+            if (!IsOwner) {
+                return;
+            }
+            _body.AddForce(_moveVector * _acceleration);
+        }
+
+
+        // Update is called once per frame
+        private void Update() {
+            if (!IsOwner) {
+                return;
+            }
+            _moveVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            _moveVector.Normalize();
+        }
+    }
+}
+
