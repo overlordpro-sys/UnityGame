eb362ae17ff216ea68afea2d6e1f837062adb879
overlordpro-sys <brent.littlebean@gmail.com>
2024-02-06 14:16:27 -0800

    Working movement script, needs tuning

--- Assets/Code/Scripts/Player/PlayerRunData.cs
+++ Assets/Code/Scripts/Player/PlayerRunData.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Player Run Data")] //Create a new playerData object by right clicking in the Project Menu then Create/Player/Player Data and drag onto the player
 public class PlayerRunData : ScriptableObject {
+    [Header("Gravity")]
+    [HideInInspector] public float gravityStrength; //Downwards force (gravity) needed for the desired jumpHeight and jumpTimeToApex.
+    [HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
+    //Also the value the player's rigidbody2D.gravityScale is set to.
+    [Space(5)]
+    public float fallGravityMult; //Multiplier to the player's gravityScale when falling.
+    public float maxFallSpeed; //Maximum fall speed (terminal velocity) of the player when falling.
+    [Space(5)]
+    public float fastFallGravityMult; //Larger multiplier to the player's gravityScale when they are falling and a downwards input is pressed.
+    //Seen in games such as Celeste, lets the player fall extra fast if they wish.
+    public float maxFastFallSpeed; //Maximum fall speed(terminal velocity) of the player when performing a faster fall.
+
     [Header("Run")]
     public float runMaxSpeed; // Target speed we want the player to reach.
     public float runAcceleration; // Time (approx.) time we want it to take for the player to accelerate from 0 to the runMaxSpeed.
@@ -13,11 +25,39 @@ public class PlayerRunData : ScriptableObject {
     [Range(0.01f, 1)] public float deccelInAir;
     public bool doConserveMomentum;
 
+    [Space(20)]
+
+    [Header("Jump")]
+    public float jumpHeight; //Height of the player's jump
+    public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
+    [HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+
+    [Header("Both Jumps")]
+    public float jumpCutGravityMult; //Multiplier to increase gravity if the player releases thje jump button while still jumping
+    [Range(0f, 1)] public float jumpHangGravityMult; //Reduces gravity while close to the apex (desired max height) of the jump
+    public float jumpHangTimeThreshold; //Speeds (close to 0) where the player will experience extra "jump hang". The player's velocity.y is closest to 0 at the jump's apex (think of the gradient of a parabola or quadratic function)
+    [Space(0.5f)]
+    public float jumpHangAccelerationMult;
+    public float jumpHangMaxSpeedMult;
+
+    [Header("Assists")]
+    [Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
+    [Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
+
 
     private void OnValidate() {
+        //Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2) 
+        gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+
+        //Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
+        gravityScale = gravityStrength / Physics2D.gravity.y;
+
         //Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
         runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
         runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+        //Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
+        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
 
         #region Variable Ranges
         runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
--- Assets/Code/Scripts/Player/PlayerRunNetwork.cs
+++ Assets/Code/Scripts/Player/PlayerRunNetwork.cs
@@ -11,35 +11,34 @@ namespace Assets.Code.Scripts.Player {
         [SerializeField] private Rigidbody2D _body;
         [SerializeField] private Collider2D _collider;
 
-        // State check
-        private bool _isFacingRight;
-        private float _lastOnGroundTime;
-        [SerializeField] private float _groundBoxXOffset; // Offset from player collider box width
-        [SerializeField] private float _groundBoxY;
-        [SerializeField] private LayerMask _groundLayer;
-
         // Input
         private Vector2 _moveDirection;
         [SerializeField] private InputActionReference _moveAction;
         [SerializeField] private InputActionReference _jumpAction;
 
-        public override void OnNetworkSpawn() {
-            if (IsOwner) {
-                _isFacingRight = true;
-            }
-        }
+        // Run
+        private float _lastOnGroundTime;
+        private bool _isFacingRight;
 
-        private void FixedUpdate() {
-            bool grounded = IsGrounded();
+        // Ground
+        [SerializeField] private float _groundBoxXOffset; // Offset from player collider box width
+        [SerializeField] private float _groundBoxY;
+        [SerializeField] private LayerMask _groundLayer;
+
+        // Jump
+        private float _lastPressedJumpTime; //  for input buffer
+        private bool _isJumping;
+        private bool _isJumpFalling;
+        private bool _isJumpCut;
+
+        public override void OnNetworkSpawn() {
             if (!IsOwner) {
                 return;
             }
-
-            Run();
-            //if (grounded && _jumpAction.action.IsPressed()) {
-            //    Debug.Log("Jumped");
-            //    _body.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
-            //}
+            _jumpAction.action.Enable();
+            _jumpAction.action.started += OnJumpDown;
+            _jumpAction.action.canceled += OnJumpUp;
+            _isFacingRight = true;
         }
 
         // Update is called once per frame
@@ -47,23 +46,73 @@ namespace Assets.Code.Scripts.Player {
             if (!IsOwner) {
                 return;
             }
+
+            // Timers
             _lastOnGroundTime -= Time.deltaTime;
+            _lastPressedJumpTime -= Time.deltaTime;
 
-            // Get x movement
-            _moveDirection = _moveAction.action.ReadValue<Vector2>();
-
+            // Input
+            _moveDirection = _moveAction.action.ReadValue<Vector2>(); // Get x movement
             if (_moveDirection.x != 0)
                 CheckDirectionToFace(_moveDirection.x > 0);
 
-            if (IsGrounded()) //checks if set box overlaps with ground
-                _lastOnGroundTime = 0.1f;
+            // Time since was last grounded for leniency
+            if (!_isJumping && IsGrounded()) { //checks if set box overlaps with ground
+                _lastOnGroundTime = Data.coyoteTime;
+            }
 
+            // Jump checks
+            if (_isJumping && _body.velocity.y < 0) { // If falling, no longer jumping
+                _isJumping = false;
+            }
+
+            if (_lastOnGroundTime > 0 && !_isJumping) { // If grounded or just grounded, reset jump cut and falling
+                _isJumpCut = false;
+                _isJumpFalling = false;
+            }
+
+            // Jump
+            if (CanJump() && _lastPressedJumpTime > 0) { // if allowed to jump and within jump input buffer time
+                _isJumping = true;
+                _isJumpCut = false;
+                _isJumpFalling = false;
+                Jump();
+            }
+
+            // Gravity
+            if (_body.velocityY < 0 && _moveDirection.y < 0) { // if falling and pressing down
+                SetGravityScale(Data.gravityScale * Data.fastFallGravityMult);
+                _body.velocity = new Vector2(_body.velocity.x, Mathf.Max(_body.velocity.y, -Data.maxFastFallSpeed));
+            }
+            else if (_isJumpCut) {
+                SetGravityScale(Data.gravityScale * Data.jumpCutGravityMult);
+                _body.velocity = new Vector2(_body.velocity.x, Mathf.Max(_body.velocity.y, -Data.maxFallSpeed));
+            }
+            else if ((_isJumping || _isJumpFalling) && Mathf.Abs(_body.velocityY) < Data.jumpHangTimeThreshold) {
+                SetGravityScale(Data.gravityScale * Data.jumpHangGravityMult);
+            }
+            else if (_body.velocity.y < 0) {
+                //Higher gravity if falling
+                SetGravityScale(Data.gravityScale * Data.fallGravityMult);
+                _body.velocity = new Vector2(_body.velocity.x, Mathf.Max(_body.velocity.y, -Data.maxFallSpeed));
+            }
+            else {
+                //Default gravity if standing on a platform or moving upwards
+                SetGravityScale(Data.gravityScale);
+            }
+        }
+        private void FixedUpdate() {
+            if (!IsOwner) {
+                return;
+            }
+
+            Run();
         }
 
         // Run
         private void Run() {
             float targetSpeed = _moveDirection.x * Data.runMaxSpeed;
-
+            targetSpeed = Mathf.Lerp(_body.velocityX, targetSpeed, 1);
             float accelRate;
 
             // Different accelerations based on grounded
@@ -72,14 +121,13 @@ namespace Assets.Code.Scripts.Player {
             else
                 accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount * Data.accelInAir : Data.runDeccelAmount * Data.deccelInAir;
 
-            /* 
+
             //Increase are acceleration and maxSpeed when at the apex of their jump, makes the jump feel a bit more bouncy, responsive and natural
-            if ((IsJumping || IsWallJumping || _isJumpFalling) && Mathf.Abs(_body.velocity.y) < Data.jumpHangTimeThreshold)
-            {
+            if ((_isJumping || _isJumpFalling) && Mathf.Abs(_body.velocityY) < Data.jumpHangTimeThreshold) {
                 accelRate *= Data.jumpHangAccelerationMult;
                 targetSpeed *= Data.jumpHangMaxSpeedMult;
             }
-            */
+
 
             //We won't slow the player down if they are moving in their desired direction but at a greater speed than their maxSpeed
             if (Data.doConserveMomentum && Mathf.Abs(_body.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(_body.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && _lastOnGroundTime < 0) {
@@ -89,7 +137,7 @@ namespace Assets.Code.Scripts.Player {
             }
 
             // Proportionally increase speed
-            float speedDif = targetSpeed - _body.velocity.x;
+            float speedDif = targetSpeed - _body.velocityX;
             float movement = speedDif * accelRate;
 
             // Apply to rigid body
@@ -105,6 +153,27 @@ namespace Assets.Code.Scripts.Player {
             _isFacingRight = !_isFacingRight;
         }
 
+        // Jump
+        private void OnJumpDown(InputAction.CallbackContext context) {
+            _lastPressedJumpTime = Data.jumpInputBufferTime;
+        }
+
+        private void OnJumpUp(InputAction.CallbackContext context) {
+            if (CanJumpCut()) {
+                _isJumpCut = true;
+            }
+        }
+        private void Jump() {
+            _lastPressedJumpTime = 0;
+            _lastOnGroundTime = 0;
+            _body.velocityY = 0;
+
+            _body.AddForceY(Data.jumpForce, ForceMode2D.Impulse);
+        }
+
+        private void SetGravityScale(float scale) {
+            _body.gravityScale = scale;
+        }
 
         // Checks
         public bool IsGrounded() {
@@ -112,6 +181,14 @@ namespace Assets.Code.Scripts.Player {
                 return false;
             }
             return Physics2D.BoxCast(_body.position, new Vector2(_collider.bounds.extents.x * 2 + _groundBoxXOffset, _groundBoxY), 0, Vector2.down, _collider.bounds.extents.y, _groundLayer);
+        }
+
+        public bool CanJump() {
+            return _lastOnGroundTime > 0 && !_isJumping; // if was grounded within coyote time
+        }
+
+        public bool CanJumpCut() {
+            return _isJumping && _body.velocityY > 0; // if jumping on the way up
         }
 
         private void OnDrawGizmos() {
