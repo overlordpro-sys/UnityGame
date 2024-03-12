fc12ebe0a6ef45619fb9438cca41cee29e02a2d6
overlordpro-sys <brent.littlebean@gmail.com>
2024-03-12 11:20:16 -0700

    Added basic 3D UI and additional lobby window

--- Assets/Lobby/TestLobby.cs
+++ Assets/Lobby/TestLobby.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+
+
 using IngameDebugConsole;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
@@ -9,7 +9,9 @@ using UnityEngine;
 
 public class TestLobby : MonoBehaviour {
     private Lobby _hostLobby;
-    // TODO ADD HEART BEAT
+
+    private float heartbeatTimerMax = 15;
+    private float heartbeatTimer;
 
     // Start is called before the first frame update
     async void Start() {
@@ -23,6 +25,12 @@ public class TestLobby : MonoBehaviour {
 
         DebugLogConsole.AddCommand("lobby.create", "Creates a test lobby", CreateLobby);
         DebugLogConsole.AddCommand("lobby.list", "List lobbies", ListLobbies);
+        DebugLogConsole.AddCommand("lobby.join_first", "Join first lobby", JoinLobbyFirst);
+        DebugLogConsole.AddCommand<string>("lobby.join_code", "Join lobby by code", JoinLobbyByCode);
+    }
+
+    private void Update() {
+        HandleLobbyHeartbeat();
     }
 
     async void CreateLobby() {
@@ -31,7 +39,8 @@ public class TestLobby : MonoBehaviour {
             int maxPlayers = 4;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
             _hostLobby = lobby;
-            Debug.Log("Lobby created: " + lobby.Name + " " + lobby.Id);
+            Debug.Log("Lobby created: " + lobby.Name + " " + lobby.Id + " " + lobby.LobbyCode);
+            PrintPlayers(_hostLobby);
         }
         catch (LobbyServiceException e) {
             Debug.LogError("Failed to create lobby: " + e.Message);
@@ -53,5 +62,42 @@ public class TestLobby : MonoBehaviour {
         }
     }
 
+    private async void JoinLobbyFirst() {
+        try {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
 
+            await Lobbies.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+        }
+        catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    private async void JoinLobbyByCode(string code) {
+        try {
+            await Lobbies.Instance.JoinLobbyByCodeAsync(code);
+            Debug.Log("Joined lobby with code " + code);
+        }
+        catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    private async void HandleLobbyHeartbeat() {
+        if (_hostLobby != null) {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f) {
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(_hostLobby.Id);
+            }
+        }
+    }
+
+    private void PrintPlayers(Lobby lobby) {
+        Debug.Log("Players in Lobby " + lobby.Name);
+        foreach (Unity.Services.Lobbies.Models.Player player in lobby.Players) {
+            Debug.Log(player.Id);
+        }
+    }
 }
