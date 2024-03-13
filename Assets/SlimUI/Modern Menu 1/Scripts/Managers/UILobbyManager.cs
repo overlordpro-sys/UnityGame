5c23459a9a5868b9d62530f0c077126c3e67b323
overlordpro-sys <brent.littlebean@gmail.com>
2024-03-13 03:09:40 -0700

    Working swap to lobby screen

---
+++ Assets/SlimUI/Modern Menu 1/Scripts/Managers/UILobbyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SlimUI.ModernMenu {
+    public class UILobbyManager : MonoBehaviour {
+
+
+
+
+
+        public void Start() {
+
+        }
+
+        public void Update() {
+        }
+
+
+
+    }
+}
